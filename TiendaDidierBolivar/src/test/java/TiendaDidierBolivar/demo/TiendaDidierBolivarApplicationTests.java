710a65d4c773ee89294316d7c3d525c3ef0d6ff5
Didier <dbolivar60804@ufide.ac.cr>
2024-10-02 20:26:36 -0600

    Primer Push
    Codigo base

---
+++ TiendaDidierBolivar/src/test/java/TiendaDidierBolivar/demo/TiendaDidierBolivarApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaDidierBolivar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDidierBolivarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
