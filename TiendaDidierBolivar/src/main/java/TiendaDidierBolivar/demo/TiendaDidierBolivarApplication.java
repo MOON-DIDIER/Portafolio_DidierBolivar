931d7e02895ce3c0ce5fd6214586d7ed81fecaa6
Didier <dbolivar60804@ufide.ac.cr>
2024-10-10 21:33:38 -0600

    Primer intento push

---
+++ TiendaDidierBolivar/src/main/java/TiendaDidierBolivar/demo/TiendaDidierBolivarApplication.java
@@ -0,0 +1,13 @@
+package TiendaDidierBolivar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDidierBolivarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDidierBolivarApplication.class, args);
+	}
+
+}
