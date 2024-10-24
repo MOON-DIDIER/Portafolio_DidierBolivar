d22c3f53c393571d7940ab95dc84a1abfadd45fe
Didier <dbolivar60804@ufide.ac.cr>
2024-10-23 19:29:46 -0600

    Semana 5
    error: 500

---
+++ TiendaDidierBolivar/src/main/java/TiendaDidierBolivar/demo/dao/CategoriaDao.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package TiendaDidierBolivar.demo.dao;
+
+/**
+ *
+ * @author rdcd2
+ */
+import TiendaDidierBolivar.demo.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface CategoriaDao extends JpaRepository <Categoria,Long>{
+}
---
+++ TiendaDidierBolivar/src/main/java/TiendaDidierBolivar/demo/service/CategoriaService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package TiendaDidierBolivar.demo.service;
+
+import TiendaDidierBolivar.demo.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    // Se obtiene un listado de categorias en un list
+    public List <Categoria> getCategorias(boolean activos);  
+}
